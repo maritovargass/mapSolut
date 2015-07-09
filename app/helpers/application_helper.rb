da87f827f81f2e21669138e358fa53b85630177b
Mario Vargas <mvargas@qantamedia.com>
2015-07-09 17:35:18 -0600

    typekit fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def typekit_include_tag twu0djh
+  javascript_include_tag("//use.typekit.net/twu0djh.js", :async => true) +
+  javascript_tag("try{Typekit.load()}catch(e){}")
+end
 end
